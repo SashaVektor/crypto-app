f8b5930d84222f36857a6966f2c7a14919e07d88
vektor <aveklic@gmail.com>
2023-01-30 22:10:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Layout, Space } from 'antd';
+import Typography from 'antd/es/typography/Typography';
+import { Link, Route, Routes } from 'react-router-dom';
+import {
+  Navbar, Homepage, Exchanges, Cryptocurrencies,
+  CryptoDetails, News
+} from './components'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white", textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar"; 
+import Homepage from "./Homepage";
+import CryptoDetails from "./CryptoDetails";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
+
+export {
+    Navbar, Homepage, CryptoDetails, Cryptocurrencies, News
+}
